d29b4206ccb96b0fb09e6bc94efda4ef70550555
Ankush Kanuganti <39840294+akanugan@users.noreply.github.com>
2023-10-19 11:58:50 -0400

    Jl testing (#5)
    * Added the jupyter notebook I'm playing in
    
    * Made some new histograms
    
    * Fixed broken plot
    
    * Made a basic ntuple merger file, used weights from ntuplizer
    
    * file list for running the script
    
    * Fixed it so it tkes into account number of events
    
    * Added the analysis script from Abhijith
    
    * Added the jupyter notebook I'm playing in
    
    * Made some new histograms
    
    * Fixed broken plot
    
    * Made a basic ntuple merger file, used weights from ntuplizer
    
    * file list for running the script
    
    * Fixed it so it tkes into account number of events
    
    * Added the analysis script from Abhijith
    
    * Removed irrelevant files
    
    * Moved into a directory
    
    * Altered the ntuple merger to work with just the names of the files
    
    ---------
    
    Co-authored-by: John Lawless <cms_utk@frizzle-07.nomad.utk.edu>
    Co-authored-by: John Lawless <jlawless@cmslpc178.fnal.gov>

---
+++ ntuple_merger/ntuple_merger.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import uproot
+import awkward as awk
+
+weights = {'QCD_HT300to500': 5.840092228107049,
+            'QCD_HT500to700': 0.5645990685276228 ,
+            'QCD_HT700to1000': 0.15253309779537536 ,
+            'QCD_HT1000to1500': 0.07186338787825997 ,
+            'QCD_HT1500to2000': 0.010317626274751761 ,
+            'QCD_HT2000toInf': 0.004261168431292373
+            }
+def QCD_sample_name(name):
+    for key in weights.keys():
+        if key in name:
+            return key
+    return None
+
+
+files = []
+with open('file_list.txt','r') as file_list:
+    for line in file_list:
+        line = line.strip()
+        files.append(line)
+
+HT = []
+
+for file in files:
+    name = QCD_sample_name(file)
+    if name is None:
+        continue
+    f = uproot.open('../output2/' + file)
+    br = f['events'].arrays()
+    HT.append([br['ak4_HT'], [weights[name] / len(br['ak4_HT'])]* len(br['ak4_HT']),name])
+
+
+plt.hist([row[0] for row in HT], weights= [row[1] for row in HT], bins= 100,stacked=True,label=[row[2] for row in HT],range=(0,3000))
+plt.xlabel("HT [GeV]")
+plt.yscale('log')
+plt.legend()
+plt.show()
