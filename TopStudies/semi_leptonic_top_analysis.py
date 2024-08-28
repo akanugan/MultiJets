e568d007fd1c38059d0736a44c5a27c8c5cf5efa
john lawless <jlawles4@vols.utk.edu>
2024-08-28 15:41:11 -0500

    More stuff

--- TopStudies/semi_leptonic_top_analysis.py
+++ TopStudies/semi_leptonic_top_analysis.py
@@ -3,8 +3,22 @@ from coffea.nanoevents import NanoEventsFactory, ScoutingNanoAODSchema
 from semi_leptonic_top_processor import SemiLeptonicTopProcessor
 import dask
 
+import matplotlib.pyplot as plt
 
-file_list = [line.strip('\n') for line in open("filelists/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8_0000.txt").readlines()] + [line.strip('\n') for line in open("filelists/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8_0001.txt").readlines()]
+import sys
+
+from distributed import Client
+from lpcjobqueue import LPCCondorCluster
+
+cluster = LPCCondorCluster()
+cluster.adapt(minimum=1, maximum=2500)
+client = Client(cluster)
+
+print("not stuck")
+
+
+#file_list = [line.strip('\n') for line in open("filelists/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8.txt").readlines()]
+file_list = [line.strip('\n') for line in open("filelists/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8_0000.txt").readlines()] + [line.strip('\n') for line in open("filelists/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8_0001.txt").readlines()] + [line.strip('\n') for line in open("filelists/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8_0002.txt").readlines()]
 file_list = ["root://cmseos.fnal.gov//" + string for string in file_list]
 
 
@@ -12,9 +26,9 @@ file_list = ["root://cmseos.fnal.gov//" + string for string in file_list]
 ScoutingNanoAODSchema.warn_missing_crossrefs = False
 
 small_events = NanoEventsFactory.from_root(
-    {file : "Events" for file in file_list[0:5]},
+    {file : "Events" for file in file_list},
     schemaclass=ScoutingNanoAODSchema,
-    metadata={"dataset": "TTto4Q_TuneCP5_13p6TeV_powheg-pythia8"},
+    metadata={"dataset": "HadronicTops"},
 ).events()
 
 p = SemiLeptonicTopProcessor()
@@ -24,3 +38,14 @@ out = p.process(small_events)
 (computed,) = dask.compute(out)
 print(computed)
 
+fig, ax = plt.subplots()
+computed["HadronicTops"]["mass"].plot1d(ax=ax)
+ax.set_yscale("log")
+ax.legend(title="Mass type")
+
+if len(sys.argv) > 1:
+    plt.savefig('plots/' + sys.argv[1] + '.png')
+else:
+    plt.savefig('plots/default.png')
+
+client.close()
--- TopStudies/semi_leptonic_top_processor.py
+++ TopStudies/semi_leptonic_top_processor.py
@@ -10,8 +10,6 @@ from coffea.dataset_tools import (
 
 import hist.dask as hda
 
-from distributed import Client
-
 class SemiLeptonicTopProcessor(processor.ProcessorABC):
     def __init__(self):
         pass
@@ -22,19 +20,25 @@ class SemiLeptonicTopProcessor(processor.ProcessorABC):
 
         h_mass = (
             hda.Hist.new
-            .StrCat(["opposite", "same"], name="sign")
-            .Log(1000, 0.2, 200., name="mass", label="$m_{\mu\mu}$ [GeV]")
+            .StrCat(["w", "top"], name="object")
+            .Log(1000, 70, 500., name="mass", label="$m_{\mu\mu}$ [GeV]")
             .Int64()
         )
 
-        cut = (ak.num(events.ScoutingMuon) == 2) & (ak.sum(events.ScoutingMuon.charge, axis=1) == 0)
-        # add first and second muon in every event together
-        dimuon = events.ScoutingMuon[cut][:, 0] + events.ScoutingMuon[cut][:, 1]
-        h_mass.fill(sign="opposite", mass=dimuon.mass)
+        cut = (ak.num(events.ScoutingJet) >= 4) & (events.ScoutingMET.pt > 40) & (ak.sum(events.ScoutingJet.particleNet_prob_b < 0.001 ,axis=1) > 1)
+        w_jet_cut = events[cut].ScoutingJet.particleNet_prob_b < 0.001 
+        b_jet_cut = events[cut].ScoutingJet.particleNet_prob_b > 0.1
 
-        cut = (ak.num(events.ScoutingMuon) == 2) & (ak.sum(events.ScoutingMuon.charge, axis=1) != 0)
-        dimuon = events.ScoutingMuon[cut][:, 0] + events.ScoutingMuon[cut][:, 1]
-        h_mass.fill(sign="same", mass=dimuon.mass)
+        non_b_jets = events[cut].ScoutingJet[w_jet_cut]
+        b_jets = events[cut].ScoutingJet[b_jet_cut]
+        w_candidate = non_b_jets[:,0] + non_b_jets[:,1]
+        top_candidate = b_jets[:,0] + w_candidate
+
+        
+
+        h_mass.fill(object="w", mass=w_candidate.mass)
+
+        h_mass.fill(object="top", mass=top_candidate.mass)
 
 
         return {
@@ -46,4 +50,3 @@ class SemiLeptonicTopProcessor(processor.ProcessorABC):
 
     def postprocess(self, accumulator):
         pass
-
