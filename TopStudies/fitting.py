4f098332acd1f20de536565bd6c74f6078623dd1
john lawless <jlawles4@vols.utk.edu>
2024-10-08 12:57:20 -0500

    Got a basic TFractionFitter working

--- TopStudies/fitting.py
+++ TopStudies/fitting.py
@@ -1,37 +1,103 @@
 """Testing out fitting methods."""
+import ctypes
 
-import matplotlib.pyplot as plt
-import uproot
-import zfit
+import ROOT
 
-file = uproot.open("truth_trial.root")
+from helper_functions import get_QCD_keys, x_sec
 
-h = file["mass"].to_hist()
-h = h["w",:]
+top = {}
+qcd = {}
+
+with ROOT.TFile.Open("tight_jet_constraint.root","READ") as mc_file:
+
+    temp_h = mc_file.mass.Get("TTbar")
+    bin_sel = temp_h.GetXaxis().FindBin("DeltaGr250")
+    top["TTto4Q"] = {
+        "hist": temp_h.ProjectionY("DGr250",bin_sel,bin_sel),
+        "weight": x_sec("TTto4Q") / mc_file.ev.Get("TTbar").GetString().Atof(),
+    }
+    top["TTto4Q"]["hist"].SetDirectory(ROOT.nullptr)
+
+    for key in get_QCD_keys():
+
+        temp_h = mc_file.mass.Get(key)
+        bin_sel = temp_h.GetXaxis().FindBin("DeltaGr250")
+
+        qcd[key] = {
+            "hist": temp_h.ProjectionY("DGr250",bin_sel,bin_sel),
+            "weight": x_sec(key) / mc_file.ev.Get(key).GetString().Atof(),
+                    }
+        qcd[key]["hist"].SetDirectory(ROOT.nullptr)
+
+qcd_hist = None
+
+for sec in qcd:
+
+    if qcd_hist is None:
+        qcd_hist = qcd[sec]["hist"] * qcd[sec]["weight"]
+    else:
+        qcd_hist = qcd_hist + qcd[sec]["hist"] * qcd[sec]["weight"]
+
+top_hist = top["TTto4Q"]["hist"] * top["TTto4Q"]["weight"]
+qcd_hist.Rebin(20)
+top_hist.Rebin(20)
+
+print(qcd_hist)
+
+fake_data_profile = (qcd_hist + 10.5 * top_hist)
+
+fake_data = ROOT.TH1F("fake_data","fake_data",50,100,300)
+rand = ROOT.TRandom3(451)
+fake_data.FillRandom(fake_data_profile,10_000_000,rand)
+
+norm = fake_data_profile.Integral() / fake_data.Integral()
+print(norm)
+fake_data.Scale(norm)
+
+mc = ROOT.TObjArray(2)
+
+mc.Add(qcd_hist)
+mc.Add(top_hist)
+
+fit = ROOT.TFractionFitter(fake_data,mc)
+
+status = fit.Fit()
 
 
-binned_data = zfit.data.BinnedData.from_hist(h)
+qcd_frac, qcd_error = ctypes.c_double(0.),ctypes.c_double(0.)
+top_frac, top_error = ctypes.c_double(0.),ctypes.c_double(0.)
+fit.GetResult(0,qcd_frac, qcd_error)
+fit.GetResult(1,top_frac, top_error)
+fdata_norm = fake_data.Integral()
+qcd_norm = qcd_hist.Integral()
+top_norm = top_hist.Integral()
+qcd_result = qcd_frac.value*(fdata_norm/qcd_norm)*qcd_hist
+top_result = top_frac.value*(fdata_norm/top_norm)*top_hist
+# result = fit.GetPlot()
+result = qcd_result + top_result
 
-binning = zfit.binned.RegularBinning(120, 60, 120, name="mass")
-space = zfit.Space("mass", binning=binning)
+c1 = ROOT.TCanvas()
+fake_data.Draw("ep")
 
-background = zfit.Parameter("background", 0)
-mu = zfit.Parameter("mu", 80)
-gamma = zfit.Parameter("gamma", 4)
-unbinned_model = zfit.pdf.SumPDF(
-    [zfit.pdf.Uniform(60, 120, space), zfit.pdf.Cauchy(mu, gamma, space)], [background],
-)
+result.SetLineColor(ROOT.kRed)
+result.Draw("same F")
+qcd_result.SetLineColor(2)
+qcd_result.Draw("same L")
+qcd_result.SetLineColor(3)
+top_result.Draw("same L")
 
-model = zfit.pdf.BinnedFromUnbinnedPDF(unbinned_model, space)
-loss = zfit.loss.BinnedNLL(model, binned_data)
-
-minimizer = zfit.minimize.Minuit()
-result = minimizer.minimize(loss)
-
-binned_data.to_hist().plot(density=1)
-model.to_hist().plot(density=1)
+legend = ROOT.TLegend()
+legend.AddEntry(fake_data, "QCD + n*Top from MC","ep")
+legend.AddEntry(result, "Total Fit result","f")
+legend.AddEntry(qcd_result, "QCD","l")
+legend.AddEntry(top_result, "TTbar","l")
+legend.Draw("same")
 
 
-plt.savefig("tester_mass_lin.png")
-
+with ROOT.TFile("fit_hists.root", "recreate") as outfile:
+    outfile.WriteObject(qcd_hist, "qcd")
+    outfile.WriteObject(top_hist, "top")
+    outfile.WriteObject(fake_data, "fake_data")
+    outfile.WriteObject(result, "fit_result")
+    outfile.WriteObject(c1,"canvas")
 
